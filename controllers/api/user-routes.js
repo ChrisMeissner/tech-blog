439d81da866443d9d3181d1cba1eadbae72a54e9
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2020-11-09 00:47:03 -0800

    add partials and helpers

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -85,6 +85,17 @@ router.post('/login', (req, res) => {
   });
 });
 
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 router.put('/:id', (req, res) => {
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
